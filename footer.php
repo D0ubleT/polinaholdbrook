99d7e1713ea59b69a549ed217ea0a38701e1929b
D0ubleT <vitto.taddeo@gmail.com>
2021-10-29 17:01:52 -0300

    actualización de footer y agregado de página en construcción

--- footer.php
+++ footer.php
@@ -1,22 +1,24 @@
 <?php
 /**
- * The footer template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package polinaholdbrook
- */
+* The footer template.
+*
+* @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+*
+* @package polinaholdbrook
+*/
 
 ?>
-		</main>
-		<footer id="colophon" class="site-footer">
-<?php
-polinaholdbrook_print_menu( 'footer' );
-?>
-			<a class="powered-by" href="https://github.com/D0ubleT/" target="_blank">
-				<span class="powered">powered by D0ubleT</span>
-			</a>
-		</footer>
-		<?php wp_footer(); ?>
-	</body>
+</main>
+<footer id="colophon" class="site-footer">
+	<?php
+	polinaholdbrook_print_menu( 'footer' );
+	?>
+	<a href="https://www.instagram.com/polina_holdbrook/" target="_blank" title="Instagram @polina_holdbrook"><i class="fab fa-instagram"></i></a>
+
+	<a class="powered-by" href="https://github.com/D0ubleT/" target="_blank">
+		<span class="powered">powered by D0ubleT</span>
+	</a>
+</footer>
+<?php wp_footer(); ?>
+</body>
 </html>
---
+++ page-en-construccion.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ALT_Cooperativa
+ */
+
+get_header();
+?>
+<section id="inicio">
+	<img src="./img/perfil.png" alt="Imagen de perfil de Polina Holdbrook">
+	<h1>Sitio en construcción.</h1>
+	<p>Pronto tendrás muchas novedades.</p>
+</section>
+<?php
+get_footer();
